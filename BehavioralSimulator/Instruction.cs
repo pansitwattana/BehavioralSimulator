81cf92efad57f6f492d8c7740359fb534ec139b3
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2016-11-07 20:23:02 +0700

    Initialize Project

---
+++ BehavioralSimulator/Instruction.cs
@@ -0,0 +1,51 @@
+﻿namespace BehavioralSimulator
+{
+    public class Instruction
+    {
+        public string field1 { get; set; }
+        public string field2 { get; set; }
+        public string field3 { get; set; }
+        public string field4 { get; set; }
+        public string field5 { get; set; }
+
+        public string OpCode
+        {
+            get
+            {
+                return field1;
+            }
+        }
+
+        public string RegA
+        {
+            get
+            {
+                return field2;
+            }
+        }
+
+        public string RegB
+        {
+            get
+            {
+                return field3;
+            }
+        }
+
+        public string DestRsg
+        {
+            get
+            {
+                return field4;
+            }
+        }
+
+        public string OffsetField
+        {
+            get
+            {
+                return field4 + field5;
+            }
+        }
+    }
+}
--- BehavioralSimulator/Program.cs
+++ BehavioralSimulator/Program.cs
@@ -8,8 +8,47 @@ namespace BehavioralSimulator
 {
     class Program
     {
+        List<Instruction> instructions = new List<Instruction>();
         static void Main(string[] args)
         {
+            Input();
+            Process();
+            Output();
+        }
+
+        private static void Output()
+        {
+            
+        }
+
+        private static void Process()
+        {
+            
+        }
+
+        private static void Input()
+        {
+            string[] textsFromFile = ReadFromFile();
+            foreach (string text in textsFromFile)
+            {
+                SplitText(DecToBin(text));
+            }
+        }
+
+        private static string DecToBin(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SplitText(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string[] ReadFromFile()
+        {
+            string[] textFromFile = new string[2] { "8454151", "9043971" };
+            return textFromFile;
         }
     }
 }
