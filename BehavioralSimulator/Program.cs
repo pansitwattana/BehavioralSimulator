fa8984c99e83cd02f00c3fc62705348d8fb16207
KAWEPHAT WONGCHAI <kawephat_w@cmu.ac.th>
2016-11-07 21:56:27 +0700

    getinputfile&converttobinary

--- BehavioralSimulator/Program.cs
+++ BehavioralSimulator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BehavioralSimulator
 {
@@ -11,7 +12,7 @@ namespace BehavioralSimulator
         List<Instruction> instructions = new List<Instruction>();
         static void Main(string[] args)
         {
-            Input();
+            Input(args);
             Process();
             Output();
         }
@@ -26,29 +27,57 @@ namespace BehavioralSimulator
             
         }
 
-        private static void Input()
+        private static void Input(string[] args)
         {
-            string[] textsFromFile = ReadFromFile();
+            string[] textsFromFile = ReadFromFile(args);
             foreach (string text in textsFromFile)
             {
                 SplitText(DecToBin(text));
+                Console.WriteLine(DecToBin(text));
             }
         }
 
         private static string DecToBin(string text)
         {
-            throw new NotImplementedException();
+            int value = Int32.Parse(text);
+            if (value >= 0)
+            {
+                string result = Convert.ToString(value, 2).PadLeft(25, '0');
+                return result;
+            }
+            else
+            {
+                string result = Convert.ToString(value, 2);
+                result = result.Substring(Math.Max(result.Length - 25, 0)).PadLeft(25, '0');
+                return result;
+            }
         }
+        
 
         private static void SplitText(string text)
         {
-            throw new NotImplementedException();
+            
         }
 
-        private static string[] ReadFromFile()
+        private static string[] ReadFromFile(string[] args)
         {
-            string[] textFromFile = new string[2] { "8454151", "9043971" };
-            return textFromFile;
+            string[] lines = System.IO.File.ReadAllLines(@args[0]);
+            //TextReader input = Console.In;
+            //if (args.Any())
+            //{
+            //    var path = args[0];
+            //    if (File.Exists(path))
+            //    {
+            //        input = File.OpenText(path);
+
+            //    }
+            //    for (string line; (line = input.ReadLine()) != null;)
+            //    {
+            //        SplitText(line);
+            //    }
+            //}
+            return lines;
+            
         }
     }
 }
